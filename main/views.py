cdc7d8e9d0d6b4b102bc7f756bd0049de16eafb5
zfirlus <zanetka.f@wp.pl>
2014-04-11 20:40:29 +0200

    logowanie

--- main/views.py
+++ main/views.py
@@ -7,3 +7,20 @@ def index(request):
     template = loader.get_template('base.html')
     context = RequestContext(request)
     return HttpResponse(template.render(context))
+
+def logowanie(request):
+     if request.method == 'POST':
+        form = FormularzLogowania(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
+            login(request,user)
+            template = get_template("base.html")
+            variables = RequestContext(request,{'user':user})
+            output = template.render(variables)
+            return HttpResponseRedirect("/")
+    else:
+    form = FormularzLogowania()
+    template = get_template("login.html")
+    variables = RequestContext(request,{'form':form})
+    output = template.render(variables)
+    return HttpResponse(output)
