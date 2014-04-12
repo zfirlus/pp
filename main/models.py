5ffff5dec1307fd61f6bd7fb06eebc09a6f24561
petrosdbz20 <petrosdbz20@wp.pl>
2014-04-12 20:25:25 +0200

    done

--- main/models.py
+++ main/models.py
@@ -4,6 +4,8 @@ import datetime
 
 class Category(models.Model):
     name = models.CharField(max_length=80)
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.name
 
 
 class User(models.Model):
@@ -15,6 +17,8 @@ class User(models.Model):
     date_created = models.DateTimeField(
         auto_now_add=True)  #Automatically set the field to now when the object is first created. Useful for creation of timestamps
     last_update = models.DateTimeField(auto_now=True)
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.login
 
 
 class Project(models.Model):
@@ -34,6 +38,8 @@ class Project(models.Model):
     visit_counter = models.IntegerField()
     category = models.ForeignKey(Category)
     user = models.ForeignKey(User)
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.title
 
 
 class Perk(models.Model):
@@ -44,6 +50,8 @@ class Perk(models.Model):
     number_available = models.IntegerField(blank=True)  #blank=True znaczy, ze wartosc moze byc null
     #jesli amount=null, to liczba danej nagrody za wsparcie jest nieograniczona
     project = models.ForeignKey(Project)
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -51,6 +59,8 @@ class Comment(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     project = models.ForeignKey(Project)
     user = models.ForeignKey(User)
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.content
 
 
 class Message(models.Model):
@@ -59,6 +69,8 @@ class Message(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     user_from = models.ForeignKey(User, related_name='message_user_from')
     user_to = models.ForeignKey(User, related_name='message_user_to')
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.subject
 
 
 class Donation(models.Model):
@@ -67,3 +79,5 @@ class Donation(models.Model):
     project = models.ForeignKey(Project)
     user = models.ForeignKey(User)
     perk = models.ForeignKey(Perk)
+    def __str__(self):  # Python 3: def __str__(self):
+        return self.amount
--- main/views.py
+++ main/views.py
@@ -1,9 +1,72 @@
+from datetime import datetime, timedelta
 from django.http import HttpResponse
 from django.template import loader, RequestContext
-
-# Create your views here.
+from main.models import Category, Project
+from django.db.models import Q, Count
 
 def index(request):
-    template = loader.get_template('categories.html')
+    #template = loader.get_template('categories.html')
+    #context = RequestContext(request)
+    #return HttpResponse(template.render(context))
+    order_by = request.GET.get('order_by', '-visit_counter')
+    key = request.GET.get('key', '')
+    projects_list = Project.objects.filter(Q(title__contains=key) | Q(full_description__contains=key)).order_by(order_by)
+    now = datetime.now().date()
+    for p in projects_list:
+        diff = p.deadline - now
+        daysLeft = diff.days
+        if daysLeft < 0:
+            daysLeft = 0
+        setattr(p, 'toEnd', daysLeft)
+    template = loader.get_template('projects.html')
+    context = RequestContext(request, {
+        'projects_list': projects_list,
+        'key' : key,
+    })
+    return HttpResponse(template.render(context))
+
+def adminUsers(request):
+    template = loader.get_template('admin_users.html')
     context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
+def adminCategories(request):
+    template = loader.get_template('admin_categories.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
+def moderator(request):
+    template = loader.get_template('moderator.html')
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+
+def categories(request):
+    order_by = request.GET.get('order_by', 'name')
+    cat_list = Category.objects.annotate(count=Count('project__id')).order_by(order_by)
+
+    template = loader.get_template('categories.html')
+    context = RequestContext(request, {
+        'cat_list': cat_list,
+    })
+    return HttpResponse(template.render(context))
+
+def projects(request, cat_id="0"):
+    order_by = request.GET.get('order_by', '-visit_counter')
+    key = request.GET.get('key', '')
+    projects_list = Project.objects.filter(Q(title__contains=key) | Q(full_description__contains=key)).order_by(order_by)
+    catId = int(cat_id)
+    if catId > 0:
+        projects_list = Project.objects.filter(category__id=catId).order_by(order_by)
+    now = datetime.now().date()
+    for p in projects_list:
+        diff = p.deadline - now
+        daysLeft = diff.days
+        if daysLeft < 0:
+            daysLeft = 0
+        setattr(p, 'toEnd', daysLeft)
+    template = loader.get_template('projects.html')
+    context = RequestContext(request, {
+        'projects_list': projects_list,
+        'key' : key,
+    })
     return HttpResponse(template.render(context))
