5a1faae48f087c5f320679341f1057c03b0ace3e
zfirlus <zanetka.f@wp.pl>
2014-04-12 21:44:26 +0200

    base

--- main/views.py
+++ main/views.py
@@ -8,19 +8,4 @@ def index(request):
     context = RequestContext(request)
     return HttpResponse(template.render(context))
 
-def logowanie(request):
-     if request.method == 'POST':
-        form = FormularzLogowania(request.POST)
-        if form.is_valid():
-            user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
-            login(request,user)
-            template = get_template("base.html")
-            variables = RequestContext(request,{'user':user})
-            output = template.render(variables)
-            return HttpResponseRedirect("/")
-    else:
-    form = FormularzLogowania()
-    template = get_template("login.html")
-    variables = RequestContext(request,{'form':form})
-    output = template.render(variables)
-    return HttpResponse(output)
+
